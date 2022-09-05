1b802bcac12f6a2d82cea9ec8be222ccbd0457a9
sayuri.hamada.zizo <hamada@zizo.ne.jp>
2022-09-05 17:17:36 +0900

    チュートリアル進行

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <h2>{{subTitle}}</h2>
+  </div>
+
+</template>
+
+<script>
+export default {
+  layout: "original",
+  data() {
+    return {
+      subTitle: "この文章はNuxt.jsの基本的な使用方法について説明しています。"
+    }
+  }
+};
+</script>
